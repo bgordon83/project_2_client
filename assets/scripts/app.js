525ca4be06d9b4fef64ef6e744690de8708aec04
bgordon83 <ben.gordon1983@gmail.com>
2019-10-18 15:40:32 -0400

    updated all.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,5 +7,11 @@
 // require('./example')
 
 $(() => {
+  $('#change-password').hide()
+  $('#sign-out').hide()
   // your JS code goes here
+  $('#sign-up').on('submit', authEvents.onSignUp)
+  $('#sign-in').on('submit', authEvents.onSignIn)
+  $('#change-password').on('submit', authEvents.onChangePassword)
+  $('#sign-out').on('submit', authEvents.onSignOut)
 })
