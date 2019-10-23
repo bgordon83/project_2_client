698a5a97a1cdaa6d15bd9e92867db44627cbe8ba
bgordon83 <ben.gordon1983@gmail.com>
2019-10-22 23:22:24 -0400

    WIP

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,11 +1,11 @@
 'use strict'
-
 // use require with a reference to bundle the file and use it in this file
 // const examprle = require('./example')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
-const events = require('./events.js')
+// const events = require('./events.js')
+import events from './events.js'
 
 $(() => {
   $('#change-password').hide()
